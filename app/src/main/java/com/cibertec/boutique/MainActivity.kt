01d601d9260f37189e63e9ece3266ed752129078
fioreellaa <vfiorellal12@gmail.com>
2024-06-29 17:41:15 -0500

    SpannableString

--- app/src/main/java/com/cibertec/boutique/MainActivity.kt
+++ app/src/main/java/com/cibertec/boutique/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.cibertec.boutique
 
+import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.StyleSpan
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tvRegistro:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,9 +26,33 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        tvRegistro = findViewById(R.id.tvRegistro)
+        val text: String = "No tienes una cuenta? Registrate"
+
+        val spannableString = SpannableString(text)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@MainActivity, InicioActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        val start = text.indexOf("Registrate")
+        val end = start + "Registrate".length
+
+        spannableString.setSpan(boldSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(clickableSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tvRegistro.text = spannableString
+        tvRegistro.movementMethod = LinkMovementMethod.getInstance()
     }
 
-
+    fun sendInicio(v: View){
+        val intent = Intent(this, InicioActivity::class.java)
+        startActivity(intent)
+    }
 
 
 }
--- app/src/main/java/com/cibertec/boutique/UsuarioActivity.kt
+++ app/src/main/java/com/cibertec/boutique/UsuarioActivity.kt
@@ -1,12 +1,15 @@
 package com.cibertec.boutique
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class UsuarioActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,4 +20,6 @@ class UsuarioActivity : AppCompatActivity() {
             insets
         }
     }
+
+
 }
